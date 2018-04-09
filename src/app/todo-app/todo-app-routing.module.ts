bf488661b2704db411b0aab531ee04ade2bf89c7
kduarte <katiaipduarte@hotmail.com>
2018-04-09 21:31:58 +0100

    4. Add routing

--- src/app/todo-app/todo-app-routing.module.ts
+++ src/app/todo-app/todo-app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TodoAppComponent } from './todo-app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 // for lazy loading
 const routes: Routes = [{
   path: '',
-  component: TodoAppComponent
+  component: TodoAppComponent,
+  children: [
+    { path: ':index', component: TodoItemComponent },
+  ]
 }];
 
 @NgModule({
